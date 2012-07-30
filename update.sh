c820be440d93a31885973dcce3b1e7bf38fb9860
leonacwa <leon.acwa@gmail.com>
2012-07-31 00:35:43 +0800

    更新每日一句话

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ "$#" != "1" ]; then
+	echo "Empty <commit mesage>"
+	echo "Usage: $0 <commit message>"
+	exit
+fi
 git add .
-git commit -m "'"$1"'"
+git commit -m "$1"
 git push origin master
