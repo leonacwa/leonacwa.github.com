99b9b113e7f1f91ab23a34e32a392da51e30c12b
leonacwa <leon.acwa@gmail.com>
2012-09-07 20:52:48 +0800

    给post模板添加了Disqus的评论功能

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Please do it by yourself"
+echo "Command;"
+
+echo "git add ."
+echo "git commit -m <message>"
+echo "git push origin master"
+echo ""
+exit
+
 if [ "$#" != "1" ]; then
 	echo "Empty <commit mesage>"
 	echo "Usage: $0 <commit message>"
